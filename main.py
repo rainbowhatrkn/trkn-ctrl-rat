bb774917ad3d7d0527dcb41802bdf9da2901bdbb
rainbowhatrkn <147452376+rainbowhatrkn@users.noreply.github.com>
2024-03-16 07:57:35 +0000

    Req

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = 'trkntrkn'
 
 # Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
